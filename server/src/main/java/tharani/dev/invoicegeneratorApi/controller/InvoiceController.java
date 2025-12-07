c346dc928d82230371921bf47ac4a6a524560627
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2025-12-07 06:07:30 +0530

    create email service in Brevo and create api for email service

--- server/src/main/java/tharani/dev/invoicegeneratorApi/controller/InvoiceController.java
+++ server/src/main/java/tharani/dev/invoicegeneratorApi/controller/InvoiceController.java
@@ -1,11 +1,16 @@
 package tharani.dev.invoicegeneratorApi.controller;
 
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import tharani.dev.invoicegeneratorApi.entity.Invoice;
+import tharani.dev.invoicegeneratorApi.service.EmailService;
 import tharani.dev.invoicegeneratorApi.service.InvoiceService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -14,6 +19,7 @@ import java.util.List;
 @CrossOrigin("*")
 public class InvoiceController {
     private final InvoiceService invoiceService;
+    private final EmailService emailService;
 
     @PostMapping
     public ResponseEntity<Invoice> saveInvoice(@RequestBody Invoice invoice){
@@ -28,5 +34,17 @@ public class InvoiceController {
     public ResponseEntity<Void> removeInvoice(@PathVariable String id){
         invoiceService.removeInvoice(id);
         return  ResponseEntity.noContent().build();
+    }
+    @PostMapping("/sendinvoice")
+    public ResponseEntity<?> sendInvoice(@RequestPart("file") MultipartFile file,
+                                         @RequestPart("email") String customerMail){
+
+        try {
+            emailService.sendInvoiceEmail(customerMail, file);
+            return ResponseEntity.ok().body("Invoice Sent Successfully");
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to send invoice.");
+        }
+
     }
 }
---
+++ server/src/main/java/tharani/dev/invoicegeneratorApi/service/EmailService.java
@@ -0,0 +1,36 @@
+package tharani.dev.invoicegeneratorApi.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    @Value("${spring.mail.properties.mail.smtp.from}")
+    private String fromEmail;
+
+    public void sendInvoiceEmail(String toEmail, MultipartFile file) throws MessagingException, IOException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(fromEmail);
+        helper.setTo(toEmail);
+        helper.setSubject("Your Invoice");
+        helper.setText("Dear customer, \n\nPlease find attached your Invoice. \n\nThank You!");
+
+        helper.addAttachment(file.getOriginalFilename(), new ByteArrayResource(file.getBytes()));
+
+        mailSender.send(message);
+    }
+}
